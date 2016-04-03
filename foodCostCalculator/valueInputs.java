fb734639449d1286999b3e719aac0e3ff0e76e23
PatrickHauke <patrick.hauke@outlook.com>
2016-04-02 23:34:07 -0400

    Update valueInputs.java
    Private final prevents a rewrite. Since the private variables taxRate and tipRate are not initialized, their default is set to 0. Since both taxPercent and tipPercent are set to final, their defaults are enforced to 0. This prevents them from being rewritten later on.

--- foodCostCalculator/valueInputs.java
+++ foodCostCalculator/valueInputs.java
@@ -4,13 +4,18 @@ package foodcostcaluculator_test;
 public class valueInputs {
     private int itemCount;
     private double taxRate = 8.875, tipRate, itemCost, preTaxMeal,finalTax,finalTip;
-    private final double taxPercent;
-    private final double tipPercent;
+    //private final double taxPercent;
+    //private final double tipPercent;
 
-    public valueInputs() {
-        this.taxPercent = taxRate/100;
-        this.tipPercent = tipRate/100;
-    }
+    //public valueInputs() {
+    //    this.taxPercent = taxRate/100;
+    //    this.tipPercent = tipRate/100;
+    //}
+    
+    // ** The commented portion breaks the code giving invalid tax and tip values. Private final prevents a rewrite. Since
+    //    the private variables taxRate and tipRate are not initialized, their default is set to 0. Since both taxPercent and
+    //    tipPercent are set to final, their defaults are enforced to 0. This prevents them from being rewritten later on.
+    
     public int getItemCount(){
         return itemCount;
     }
